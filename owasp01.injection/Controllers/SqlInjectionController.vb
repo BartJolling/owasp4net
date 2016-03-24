0de498a2cda390d498c7c99d79e584369a8f4768
BartJolling <bart.jolling@outlook.com>
2016-03-24 06:54:55 +0100

    example restrict user permissions on DB and basic input validation

--- owasp01.injection/Controllers/SqlInjectionController.vb
+++ owasp01.injection/Controllers/SqlInjectionController.vb
@@ -1,23 +1,30 @@
-﻿Imports System.Data.SqlClient
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.Data.SqlClient
 Imports System.Web.Mvc
 
 Namespace Controllers
     Public Class SqlInjectionController
         Inherits Controller
 
-        ' GET: SqlInjection
-        Function Index() As ActionResult
+        Public Function Index() As ActionResult
+            Return Unsafe()
+        End Function
+
+#Region "Vulnerable"
+
+        ' GET: SqlInjection/Unsafer
+        Function Unsafe() As ActionResult
+            Dim connectionString = ConfigurationManager.ConnectionStrings("NorthWindReadWrite").ConnectionString
 
             ViewData("url") = Server.UrlDecode(Request.Url.ToString())
-            Dim categoryId As String = Request.QueryString("CategoryId")
+            Dim categoryId As String = Request.QueryString("CategoryId") 'Not type safe
 
-            Dim products = LoadProducts(categoryId)
-            Return View(products)
+            Dim products = LoadProductsUnsafe(connectionString, categoryId)
+            Return View("Index", products)
 
         End Function
 
-        Private Function LoadProducts(CategoryId As String) As IEnumerable(Of Product)
-            Dim connectionString = ConfigurationManager.ConnectionStrings("NorthWind").ConnectionString
+        Private Function LoadProductsUnsafe(connectionString As String, CategoryId As String) As IEnumerable(Of Product)
 
             Dim products As New List(Of Product)
 
@@ -50,61 +57,89 @@ Namespace Controllers
 
             Return products
         End Function
+#End Region
 
-        ' GET: SqlInjection/Details/5
-        Function Details(ByVal id As Integer) As ActionResult
-            Return View()
+#Region "Safer"
+        ' GET: SqlInjection/Safer1?CategoryId=1
+        Function Safer1(categoryId As String) As ActionResult
+            Dim connectionString = ConfigurationManager.ConnectionStrings("NorthWindReadWrite").ConnectionString
+
+            ViewData("url") = Server.UrlDecode(Request.Url.ToString())
+
+            Dim products = LoadProductsUnsafe(connectionString, categoryId)
+            Return View("Index", products)
+
         End Function
 
-        ' GET: SqlInjection/Create
-        Function Create() As ActionResult
-            Return View()
+        ' GET: SqlInjection/Safer2?CategoryId=1
+        Function Safer2(categoryId As String) As ActionResult
+            Dim connectionString = ConfigurationManager.ConnectionStrings("NorthWindReadWrite").ConnectionString
+
+            ViewData("url") = Server.UrlDecode(Request.Url.ToString())
+
+            Dim products = LoadProductsUnsafe(connectionString, categoryId)
+            Return View("Index", products)
+
+        End Function
+#End Region
+
+#Region "Safe"
+        ' GET: SqlInjection/Safe?CategoryId=1
+        Function Safe(categoryModel As CategoryModel) As ActionResult
+            Dim connectionString = ConfigurationManager.ConnectionStrings("NorthWindReadWrite").ConnectionString
+
+            ViewData("url") = Server.UrlDecode(Request.Url.ToString())
+
+            If ModelState.IsValid Then
+                Dim products = LoadProductsSafe(connectionString, categoryModel.categoryId)
+                Return View("Index", products)
+            Else
+                Return View("Index", Enumerable.Empty(Of Product))
+            End If
+
         End Function
 
-        ' POST: SqlInjection/Create
-        <HttpPost()>
-        Function Create(ByVal collection As FormCollection) As ActionResult
-            Try
-                ' TODO: Add insert logic here
 
-                Return RedirectToAction("Index")
-            Catch
-                Return View()
-            End Try
+        Private Function LoadProductsSafe(connectionString As String, CategoryId As String) As IEnumerable(Of Product)
+
+            Dim products As New List(Of Product)
+
+            Dim query As String = "SELECT * FROM Products"
+
+            If Not String.IsNullOrWhiteSpace(CategoryId) Then
+                query &= " WHERE CategoryId = " & CategoryId
+            End If
+
+            Using connection = New SqlConnection(connectionString)
+                Dim command As New SqlCommand(query, connection)
+                command.Connection.Open()
+                Dim reader = command.ExecuteReader()
+
+                While reader.Read()
+                    products.Add(New Product() With {
+                        .ProductID = reader(0),
+                        .ProductName = If(IsDBNull(reader(1)), -1, reader(1)),
+                        .SupplierID = If(IsDBNull(reader(2)), -1, reader(2)),
+                        .CategoryID = If(IsDBNull(reader(3)), -1, reader(3)),
+                        .QuantityPerUnit = If(IsDBNull(reader(4)), -1, reader(4)),
+                        .UnitPrice = If(IsDBNull(reader(5)), -1, reader(5)),
+                        .UnitsInStock = If(IsDBNull(reader(6)), -1, reader(6)),
+                        .UnitsOnOrder = If(IsDBNull(reader(7)), -1, reader(7)),
+                        .ReorderLevel = If(IsDBNull(reader(8)), -1, reader(8)),
+                        .Discontinued = If(IsDBNull(reader(9)), -1, reader(9))
+                    })
+                End While
+            End Using
+
+            Return products
         End Function
+#End Region
 
-        ' GET: SqlInjection/Edit/5
-        Function Edit(ByVal id As Integer) As ActionResult
-            Return View()
-        End Function
-
-        ' POST: SqlInjection/Edit/5
-        <HttpPost()>
-        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
-            Try
-                ' TODO: Add update logic here
-
-                Return RedirectToAction("Index")
-            Catch
-                Return View()
-            End Try
-        End Function
-
-        ' GET: SqlInjection/Delete/5
-        Function Delete(ByVal id As Integer) As ActionResult
-            Return View()
-        End Function
-
-        ' POST: SqlInjection/Delete/5
-        <HttpPost()>
-        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
-            Try
-                ' TODO: Add delete logic here
-
-                Return RedirectToAction("Index")
-            Catch
-                Return View()
-            End Try
-        End Function
     End Class
+
+    Public Class CategoryModel
+        <StringLength(3)>
+        Public Property categoryId As String
+    End Class
+
 End Namespace
---
+++ owasp01.injection/SQL/TestTable.sql
@@ -0,0 +1,24 @@
+﻿USE [Northwind]
+GO
+
+DROP TABLE [dbo].[TestTable]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[TestTable](
+	[Key] [int] NOT NULL,
+	[Value] [varchar](50) NOT NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
